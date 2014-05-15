9d7e75c178b341494826e5812e3831a78decddc5
Edgar Neto <edgar.figueiredo@gmail.com>
2014-05-16 00:23:52 +0200

    Added first draft of newsfeed layout

--- public/javascripts/FeedCtrl.js
+++ public/javascripts/FeedCtrl.js
@@ -1,4 +1,26 @@
 function FeedCtrl($scope)
 {
-	$scope.greet = " Haaaaloooo ";
+	$scope.posts = [
+		{description: 'Isso eh revoltante',
+		 link:'https://i.imgur.com/9fXIbBz.jpg',
+		 karma:100},
+		{description: 'Isso aconteceu',
+		 link:'https://www.youtube.com/watch?v=Kd9QO-Ktix0',
+		 karma:1000}
+	];
+
+
+	$scope.voteUp = function(post)
+	{
+		post.karma += 1;
+		$scope.save();
+		$scope.sort();
+	};
+
+	$scope.voteDown = function(post)
+	{
+		post.karma -= 1;
+		$scope.save();
+		$scope.sort();
+	};
 }
